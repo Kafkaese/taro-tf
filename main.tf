41d90fc7982751a49603df98d1ad6021b19e9521
Frederik Laubisch <kafkaese@gmail.com>
2023-06-26 12:11:20 +0200

    add azure postgresql server

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = var.resource_group_name
 }
+
+resource "azurerm_postgresql_server" "pg-server" {
+  name = "taro-server"
+  location = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  sku_name = "B_Gen4_1"
+  storage_mb = 5120
+  version = 11
+  ssl_enforcement_enabled = true
+}
