ace6f64dec7ab1f52c2ed38ec4885666784bccf1
Frederik Laubisch <kafkaese@gmail.com>
2023-06-26 13:12:52 +0200

    fix sql server sku and dependency on rg

--- main.tf
+++ main.tf
@@ -7,11 +7,12 @@ resource "azurerm_postgresql_server" "pg-server" {
   name = "taro-server"
   location = var.resource_group_location
   resource_group_name = var.resource_group_name
-  sku_name = "B_Gen4_1"
+  sku_name = "B_Gen5_1"
   storage_mb = 5120
   version = 11
   ssl_enforcement_enabled = true
   auto_grow_enabled = false
   administrator_login = var.postgres_user
   administrator_login_password = var.postgres_password
+  depends_on = [ azurerm_resource_group.rg ]
 }
