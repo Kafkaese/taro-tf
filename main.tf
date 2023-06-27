8026a28483f25141d783103a7498d6f6b45cdf3a
Frederik Laubisch <kafkaese@gmail.com>
2023-06-27 09:30:06 +0200

    switch to postgresql flexible server + db

--- main.tf
+++ main.tf
@@ -3,23 +3,20 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
 }
 
-resource "azurerm_postgresql_server" "pg-server" {
+resource "azurerm_postgresql_flexible_server" "pg-server" {
   name = "taro-server"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name = "B_Gen5_1"
-  storage_mb = 5120
+  sku_name = "B_Standard_B1ms"
+  storage_mb = 32768
   version = 11
-  ssl_enforcement_enabled = true
-  auto_grow_enabled = false
   administrator_login = var.postgres_user
-  administrator_login_password = var.postgres_password
+  administrator_password = var.postgres_password
 }
 
-resource "azurerm_postgresql_database" "pg-db" {
+resource "azurerm_postgresql_flexible_server_database" "pg-db" {
   name = var.postgres_database
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name = azurerm_postgresql_server.pg-server.name
+  server_id = azurerm_postgresql_flexible_server.pg-server.id
   charset = "UTF8"
-  collation = "en-US"
+  collation = "en_US.utf8"
 }
