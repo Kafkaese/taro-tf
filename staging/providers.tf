ebee94e2a357feb11d335cc51890cab825bf637d
Frederik Laubisch <kafkaese@gmail.com>
2023-07-25 13:07:50 +0200

    add azurerm backend for tf state

--- staging/providers.tf
+++ staging/providers.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">=0.12"
 
+  backend "azurerm" {
+    resource_group_name  = "taro"
+    storage_account_name = "taro"
+    container_name       = "terraform-staging-env"
+    key                  = "staging.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
