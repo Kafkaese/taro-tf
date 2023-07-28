e34859b5ce5bc3595585635150c73a6df069b0d8
Frederik Laubisch <kafkaese@gmail.com>
2023-07-28 22:09:56 +0200

    add postgres server login to outputs

--- staging/outputs.tf
+++ staging/outputs.tf
@@ -1,3 +1,7 @@
 output "acr_login" {
   value = azurerm_container_registry.container-registry.login_server
 }
+
+output "postgres_server" {
+    value = "${azurerm_postgresql_flexible_server.pg-server.name}.postgres.database.azure.com"
+}
