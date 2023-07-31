9544e98a840a1f8faa90063fc57885ae7ec14735
Frederik Laubisch <kafkaese@gmail.com>
2023-07-31 15:21:29 +0200

    add resources to outputs for persistence

--- staging/outputs.tf
+++ staging/outputs.tf
@@ -13,3 +13,19 @@ output "rg-name" {
 output "rg-location" {
   value = azurerm_resource_group.rg.location
 }
+
+output "pg-server" {
+  value = azurerm_postgresql_flexible_server.pg-server.name
+}
+
+output "pg-db" {
+  value = azurerm_postgresql_flexible_server_database.pg-db.name
+}
+
+output "pg-firewall" {
+  value = azurerm_postgresql_flexible_server_firewall_rule.pg-server-open.name
+}
+
+output "acr" {
+  value = azurerm_container_registry.container-registry.name
+}
