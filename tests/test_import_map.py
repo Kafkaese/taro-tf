e03a66ae4055a9c78d9a310bace11040419f1f38
Frederik Laubisch <kafkaese@gmail.com>
2023-08-09 12:02:18 +0200

    add test for contents of hover box tooltip

--- tests/test_import_map.py
+++ tests/test_import_map.py
@@ -40,5 +40,40 @@ def test_hover_toolip():
     
     driver.quit()
     
+def test_hover_toolip_data():
+    
+    # Open app
+    driver = webdriver.Firefox(options=firefox_options)
+     # Replace with the desired website URL
+    driver.get(url)
+
+    # Wait for app to load
+    time.sleep(1)
+    
+    # Hover over random country
+    try:
+        geography = driver.find_element(By.CLASS_NAME, 'rsm-geography')
+        actions = ActionChains(driver)
+        actions.move_to_element(geography).perform()
+    except Exception as e:
+        print(f"Error: {e}")
+        
+    # Find hover box container
+    time.sleep(1)
+    try:
+        container = driver.find_element(By.CLASS_NAME, 'hover-box-container')
+    except Exception as e:
+        print(f"Error: {e}")
+        
+    
+    # Check country name
+    assert container.find_element(By.TAG_NAME, 'h3').text == "Afghanistan"
+    assert container.find_element(By.CLASS_NAME, 'money').text != "no data"
+    assert container.find_element(By.CLASS_NAME, 'money-label').text != ""
+    
+    
+    
+    driver.quit()
+    
 if __name__ == '__main__':
-    test_hover_toolip()
+    test_hover_toolip_data()
