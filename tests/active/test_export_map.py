6bafe23c59a0d55a9611c6008a686321ae112d0c
Frederik Laubisch <kafkaese@gmail.com>
2023-08-10 11:42:04 +0200

    move active tests

--- tests/active/test_export_map.py
+++ tests/active/test_export_map.py
@@ -54,6 +54,50 @@ def test_hover_toolip():
     
     # quit webdriver
     driver.quit()
+
+def test_hover_toolip_data():
+    
+    # Open app
+    driver = webdriver.Firefox(options=firefox_options)
+     # Replace with the desired website URL
+    driver.get(url)
+
+    # Wait for app to load
+    time.sleep(1)
+    
+    # Switch to export map
+    try:
+        toggle = driver.find_element(By.CLASS_NAME, 'toggle')
+        export_button = toggle.find_element(By.CLASS_NAME, 'text-box')
+        export_button.click()
+    except Exception as e:
+        print(f"Error: {e}")
+    
+    
+    # Hover over France
+    try:
+        geography = driver.find_elements(By.CLASS_NAME, 'rsm-geography')[56]
+        actions = ActionChains(driver)
+        actions.move_to_element(geography).perform()
+    except Exception as e:
+        print(f"Error: {e}")
+        
+    # Find hover box container
+    time.sleep(1)
+    try:
+        container = driver.find_element(By.CLASS_NAME, 'hover-box-container')
+    except Exception as e:
+        print(f"Error: {e}")
+        
+    
+    # Check country name and data
+    assert container.find_element(By.TAG_NAME, 'h3').text == "France"
+    assert container.find_element(By.CLASS_NAME, 'money').text != "no data"
+    assert container.find_element(By.CLASS_NAME, 'money-label').text != ""
+    
+    
+    
+    driver.quit()
     
 if __name__ == '__main__':
     test_hover_toolip()
