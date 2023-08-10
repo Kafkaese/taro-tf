1a82c15ac41257e74e151bceca11c397dd11715d
Frederik Laubisch <kafkaese@gmail.com>
2023-08-10 11:39:28 +0200

    move sidebar tests to archive

--- tests/archived/test_import_sidebar.py
+++ tests/archived/test_import_sidebar.py
@@ -1,29 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
 
 
 # Configure Firefox options for headless mode
-firefox_options = webdriver.FirefoxOptions()
+firefox_options = webdriver.ChromeOptions()
 firefox_options.add_argument('--headless')
 
 # Path for logs
 log_path = os.environ['LOG_PATH'] + '/geckodriver.log'
 
 # App url
-url = f"{os.environ['REACT_HOST']}:{os.environ['REACT_PORT']}"
+url = f"http://{os.environ['REACT_HOST']}:{os.environ['REACT_PORT']}"
 
 def test_sidebar():
     
     # Open app
-    driver = webdriver.Firefox()
-     # Replace with the desired website URL
+    driver = webdriver.Chrome(options=firefox_options)
+    
+    # Get app
+    print(url)
     driver.get(url)
 
     # Wait for app to load
-    time.sleep(1)
+    time.sleep(10)
     
     # Click on Afghanistan
     try:
@@ -32,19 +36,26 @@ def test_sidebar():
     except Exception as e:
         print(f"Error: {e}")
         
+    # Sleep for headless
+    wait = WebDriverWait(driver, 10)
+    sidebar = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'sideBar')))
+
+    
     # Find sidebar elements
     try:
-        sidebar  = driver.find_element(By.CLASS_NAME, 'sideBar')
+        #sidebar  = driver.find_element(By.CLASS_NAME, 'sideBar')
         title = sidebar.find_element(By.CLASS_NAME, 'title')
     except Exception as e:
         print(f"Error: {e}")
-        
-    # Sleep for headless
-    time.sleep(10)
     
+    print(title)
+    print(title.tag_name)
     print(title.text)
     # Check values
     assert title.text == 'Afghanistan'
+    
+    # Quit driver
+    driver.quit()
     
 if __name__ == '__main__':
     test_sidebar()
