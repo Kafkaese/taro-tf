e8d6d29b9ef5c6208ddfbd304915c3c67358499f
Frederik Laubisch <kafkaese@gmail.com>
2023-10-04 13:43:32 +0200

    remove acr from presitent prod config

--- production/persistent/main.tf
+++ production/persistent/main.tf
@@ -10,11 +10,3 @@ resource "azurerm_storage_account" "storage" {
   account_tier = "Standard"
   account_replication_type = "LRS"
 }
-
-resource "azurerm_container_registry" "container-registry" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
-}
-
