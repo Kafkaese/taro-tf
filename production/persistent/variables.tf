c4bf29c2cf8a0876752dc95bd02cab161de60c11
Frederik Laubisch <kafkaese@gmail.com>
2023-10-04 13:12:20 +0200

    delete legacy vars

--- production/persistent/variables.tf
+++ production/persistent/variables.tf
@@ -8,20 +8,6 @@ variable "resource_group_name" {
   description = "The resource group name."
 }
 
-variable "postgres_user" {
-  default = "postgres"
-  sensitive = true
-}
-
-variable "postgres_password" {
-  default = "secret"
-  sensitive = true
-}
-
-variable "postgres_database" {
-  default = "taro-db"
-}
-
 variable "acr_name" {
-  default = "taro-container-registry  "
+  default = "taro-container-registry"
 }
