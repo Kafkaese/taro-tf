3a30ea3b5f26103720ca7f68b07b3705002b8eaa
Frederik Laubisch <kafkaese@gmail.com>
2023-10-05 14:36:41 +0200

    enable admin on containe registry

--- production/persistent/main.tf
+++ production/persistent/main.tf
@@ -20,4 +20,6 @@ resource "azurerm_container_registry" "container-registry" {
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
   sku                 = "Basic"
+  admin_enabled       = true
 }
+
