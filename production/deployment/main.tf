3015152ffe36864bd3885ba8ea864b3e7fa98945
Frederik Laubisch <kafkaese@gmail.com>
2023-10-30 12:56:28 +0100

    feat: add subnet delegation for api container group

--- production/deployment/main.tf
+++ production/deployment/main.tf
@@ -53,10 +53,19 @@ resource "azurerm_private_dns_zone_virtual_network_link" "taro_vnete_dns_zone" {
 
 # Subnet for the API
 resource "azurerm_subnet" "backend_subnet" {
-  name = "taro-production-backend-subnet"
+  name                 = "taro-production-backend-subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.taro_production_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+  delegation {
+    name = "api"
+    service_delegation {
+      name    = "Microsoft.ContainerInstance"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
 }
 
 
