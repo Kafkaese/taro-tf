920854f4b5e9f73ade6fc55247ac0e3cc24f3ddb
Frederik Laubisch <kafkaese@gmail.com>
2023-11-01 14:52:30 +0100

    fix: make public ip sku standard to work with standard sku load balancer

--- production/persistent/main.tf
+++ production/persistent/main.tf
@@ -46,6 +46,7 @@ resource "azurerm_public_ip" "taro-production-api-public-ip" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Static"
+  sku                 = "Standard"
 
   tags = {
     environment = "Production"
