e52a4a9907fdc25c5991ef3c5042de67d81b1273
Frederik Laubisch <kafkaese@gmail.com>
2023-11-01 14:51:58 +0100

    fix: use standard sku load balancer to enable backend address pool

--- production/deployment/main.tf
+++ production/deployment/main.tf
@@ -133,6 +133,7 @@ resource "azurerm_lb" "taro-production-lb" {
   name                = "taro-production-load-balancer"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
+  sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
