c495d5323947a9e7039032032518979111241e56
Frederik Laubisch <kafkaese@gmail.com>
2023-11-07 14:37:40 +0100

    deat: add frontend load balancer setup to prod config

--- production/deployment/main.tf
+++ production/deployment/main.tf
@@ -85,49 +85,6 @@ resource "azurerm_subnet" "backend_subnet" {
   }
 }
 
-/*
-# Container Instance for the frontend
-resource "azurerm_container_group" "container-instance-frontend" {
-  name                = var.container_group_name_frontend
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
-  ip_address_type     = "Public"
-  os_type             = "Linux"
-  image_registry_credential {
-    username = var.container_registry_credential_user
-    password = var.container_registry_credential_password
-    server   = var.container_registry_login_server
-  }
-
-  container {
-    name   = "taro-frontend"
-    image  = "${var.container_registry_login_server}/taro:frontend"
-    cpu    = "0.5"
-    memory = "1.5"
-    environment_variables = {
-      ENV=var.environment
-      REACT_APP_API_HOST=azurerm_container_group.container-instance-api.ip_address
-      REACT_APP_API_PORT=var.api_port
-    }
-
-    ports {
-      port     = 80
-      protocol = "TCP"
-    }
-  }
-
-  tags = {
-    environment = var.environment
-  }
-
-  lifecycle {
-    replace_triggered_by = [
-      null_resource.always_run
-    ]
-  }
-}
-*/
-
 # Load balancer for API
 resource "azurerm_lb" "taro-production-lb" {
   name                = "taro-production-load-balancer"
@@ -227,3 +184,99 @@ resource "azurerm_container_group" "container-instance-api" {
   }
 }
 
+# Subnet for the Frontend
+resource "azurerm_subnet" "frontend_subnet" {
+  name                 = "taro-production-frontend-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.taro_production_vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+  delegation {
+    name = "frontend"
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+
+# Load balancer for Frontend
+resource "azurerm_lb" "taro-production-frontend-lb" {
+  name                = "taro-production-frontend-load-balancer"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicFrontendIPAddress"
+    public_ip_address_id = var.frontend_ip_id
+  }
+}
+
+# IP address pool for frontend load balancer
+resource "azurerm_lb_backend_address_pool" "taro-production-frontend-lb-address-pool" {
+  name            = "taro-frontend-pool"
+  loadbalancer_id = azurerm_lb.taro-production-frontend-lb.id
+}
+
+# Frontend address for lb address pool
+resource "azurerm_lb_backend_address_pool_address" "taro-production-frontend-container-ip-address" {
+  name                    = "taro-production-frontend-container-ip-address"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.taro-production-frontend-lb-address-pool.id
+  virtual_network_id      = azurerm_virtual_network.taro_production_vnet.id
+  ip_address              = azurerm_container_group.container-instance-frontend.ip_address
+}
+
+# Load balancer rule
+resource "azurerm_lb_rule" "taro-production-frontend-lb-rule" {
+  loadbalancer_id                = azurerm_lb.taro-production-frontend-lb.id
+  name                           = "HTTP"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 3000
+  frontend_ip_configuration_name = "PublicFrontendIPAddress"
+  backend_address_pool_ids       = [ azurerm_lb_backend_address_pool.taro-production-frontend-lb-address-pool.id ]
+}
+
+# Container Instance for the frontend
+resource "azurerm_container_group" "container-instance-frontend" {
+  name                = var.container_group_name_frontend
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  subnet_ids          = [ azurerm_subnet.frontend_subnet.id ]
+  ip_address_type     = "Public"
+  os_type             = "Linux"
+  image_registry_credential {
+    username = var.container_registry_credential_user
+    password = var.container_registry_credential_password
+    server   = var.container_registry_login_server
+  }
+
+  container {
+    name   = "taro-frontend"
+    image  = "${var.container_registry_login_server}/taro:frontend"
+    cpu    = "0.5"
+    memory = "1.5"
+    environment_variables = {
+      ENV=var.environment
+      REACT_APP_API_HOST=var.api_ip_address
+      REACT_APP_API_PORT=var.api_port
+    }
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = var.environment
+  }
+
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.always_run
+    ]
+  }
+}
