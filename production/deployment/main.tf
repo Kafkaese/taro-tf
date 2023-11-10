8c35ca8e7effc2852de95bff471edb3cff5099e9
Frederik Laubisch <kafkaese@gmail.com>
2023-11-10 13:00:14 +0100

    style: indentation in resource paramaters

--- production/deployment/main.tf
+++ production/deployment/main.tf
@@ -297,11 +297,11 @@ resource "azurerm_container_group" "container-instance-frontend" {
 
 # Storage account for certificates
 resource "azurerm_storage_account" "ssl-storage" {
-  name = "tarossl"
-  resource_group_name = var.resource_group_name
-  location = var.resource_group_location
-  account_tier = "Standard"
-  account_replication_type = "LRS"
+  name                          = "tarossl"
+  resource_group_name           = var.resource_group_name
+  location                      = var.resource_group_location
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
   public_network_access_enabled = false
 }
 
