60f0daf709a142a10ea2ad4064a485b147f46f63
Frederik Laubisch <kafkaese@gmail.com>
2023-11-22 10:06:24 +0100

    refactor: Remove legacy outputs for api and frontend public IPs

--- production/persistent/outputs.tf
+++ production/persistent/outputs.tf
@@ -6,22 +6,6 @@ output "container_registry_name" {
   value = azurerm_container_registry.container-registry.name
 }
 
-output "api_public_ip_id" {
-  value = azurerm_public_ip.taro-production-api-public-ip.id
-}
-
-output "api_public_ip_address" {
-  value = azurerm_public_ip.taro-production-api-public-ip.ip_address
-}
-
-output "frontend_public_ip_id" {
-  value = azurerm_public_ip.taro-production-frontend-public-ip.id
-}
-
-output "frontend_public_ip_address" {
-  value = azurerm_public_ip.taro-production-frontend-public-ip.ip_address
-}
-
 output "rp_vm_public_ip_id" {
   value = azurerm_public_ip.taro-production-reverse-proxy-public-ip.id
 }
