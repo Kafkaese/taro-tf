bce7d87de1dd034c5104ce820449664ca73a2931
Kafkaese <18165334+Kafkaese@users.noreply.github.com>
2026-07-23 16:04:23 +0200

    add gcp bucket name to outputs

--- production/persistent/outputs.tf
+++ production/persistent/outputs.tf
@@ -21,3 +21,13 @@ output "github_actions_pipeline_build_role_arn" {
 output "github_actions_api_build_role_arn" {
   value = aws_iam_role.github_actions_api_build.arn
 }
+
+output "frontend_bucket_name" {
+  value = google_storage_bucket.frontend.name
+}
+
+# The plain-HTTP endpoint Cloudflare will proxy to - not the bucket's GCS
+# console URL.
+output "frontend_website_endpoint" {
+  value = "http://${google_storage_bucket.frontend.name}.storage.googleapis.com"
+}
